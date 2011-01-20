4b9d02ca4c264b869ed4e4c2f9f3286e0295108c
Fabio Zendhi Nagao <nagaozen@hotmail.com>
2011-01-20 10:34:02 -0200

    integrated with GTKSourceCompletion

--- autocomplete/__init__.py
+++ autocomplete/__init__.py
@@ -1,6 +1,7 @@
-# gEdit AutoComplete
-# Copyright (C) 2009 Fabio Zendhi Nagao, Vincent Petithory
-# Copyright (C) 2006 Alin Avasilcutei, Osmo Salomaa
+# -*- coding: utf-8 -*-
+
+# gEdit AutoComplete (gedit-json-completion)
+# Copyright (C) 2011 Fabio Zendhi Nagao
 # 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -16,30 +17,65 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import gedit
+import jsonprovider
 
-from plugin import AutoComplete
-from config import ConfigurationDialog
+class JSONCompletionWindowHelper:
+    def __init__(self, plugin, window):
+        self._window = window
+        self._plugin = plugin
+        
+        self._provider = jsonprovider.JSONProvider(plugin)
+        
+        # Add the provider to all the views
+        for view in self._window.get_views():
+            self.add_view(view)
+        
+        self._tab_added_id = self._window.connect('tab-added', self.on_tab_added)
+        self._tab_removed_id = self._window.connect('tab-removed', self.on_tab_removed)
+    
+    def deactivate(self):
+        # Remove the provider from all the views
+        for view in self._window.get_views():
+            view.get_completion().completion.remove_provider(self._provider)
+        
+        self._window.disconnect(self._tab_added_id)
+        self._window.disconnect(self._tab_removed_id)
+        
+        self._window = None
+        self._plugin = None
+    
+    def update_ui(self):
+        pass
+    
+    def add_view(self, view):
+        view.get_completion().add_provider(self._provider)
+    
+    def remove_view(self, view):
+        view.get_completion().remove_provider(self._provider)
+    
+    def on_tab_added(self, window, tab):
+        # Add provider to the new view
+        self.add_view(tab.get_view())
+    
+    def on_tab_removed(self, window, tab):
+        # Remove provider from the view
+        self.remove_view(tab.get_view())
 
-class AutoCompletePlugin(gedit.Plugin):
+class JSONCompletionPlugin(gedit.Plugin):
+    WINDOW_DATA_KEY = "JSONCompletionPluginWindowData"
     
     def __init__(self):
         gedit.Plugin.__init__(self)
-        self._instances = {}
     
     def activate(self, window):
-        self._instances[window] = AutoComplete(self, window)
+        helper = JSONCompletionWindowHelper(self, window)
+        window.set_data(self.WINDOW_DATA_KEY, helper)
     
     def deactivate(self, window):
-        self._instances[window].deactivate()
-        del self._instances[window]
+        window.get_data(self.WINDOW_DATA_KEY).deactivate()
+        window.set_data(self.WINDOW_DATA_KEY, None)
     
     def update_ui(self, window):
-        self._instances[window].update_ui()
-    
-    def is_configurable(self):
-        return True
-    
-    def create_configure_dialog(self):
-        dialog = ConfigurationDialog(self)
-        return dialog
-    
+        window.get_data(self.WINDOW_DATA_KEY).update_ui()
+
+# ex:ts=4:et:
